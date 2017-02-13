5cd421e611efa40fc4d5db0da0a7c3d362bfd838
jkwong888 <jkwong888@users.noreply.github.com>
2017-02-13 10:18:37 -0500

    Integration (#1)
    * Springboot implementation (#1)
    
    * springboot implementation
    
    * use FeignClient to get and call customer service
    
    * fix feign client dependencies
    
    * add feign package
    
    * add application.yml
    
    * make authorization header optional
    
    * don't register with eureka
    
    * fix customer client
    
    * change context path to /
    
    * Set authenticated credential to customer ID
    
    * Update README.md
    
    * bluemix toolchain
    
    * remove unneeded properties from deployment
    
    * Springboot (#2)
    
    * springboot implementation
    
    * use FeignClient to get and call customer service
    
    * fix feign client dependencies
    
    * add feign package
    
    * add application.yml
    
    * make authorization header optional
    
    * don't register with eureka
    
    * fix customer client
    
    * change context path to /
    
    * Set authenticated credential to customer ID
    
    * Update README.md
    
    * bluemix toolchain
    
    * remove unneeded properties from deployment
    
    * set context path to root

---
+++ src/main/java/auth/Customer.java
@@ -0,0 +1,26 @@
+package auth;
+
+public class Customer {
+
+	private String customerId;
+	private String username;
+	private String password;
+	public String getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(String customerId) {
+		this.customerId = customerId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
