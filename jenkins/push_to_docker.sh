3c90d66a1342cabb4fadd9df15eb221f8f34a5a6
Jeffrey Kwong <jkwong@Jeffreys-MacBook-Pro.local>
2017-04-19 11:20:32 -0500

    fix push script

--- jenkins/push_to_docker.sh
+++ jenkins/push_to_docker.sh
@@ -7,15 +7,25 @@ cf_account=$(cat /var/run/secrets/bx-auth-secret/CF_ACCOUNT)
 cf_org=$(cat /var/run/secrets/bx-auth-secret/CF_ORG)
 cf_space=$(cat /var/run/secrets/bx-auth-secret/CF_SPACE)
 
+build_number=${BUILD_NUMBER}
+registry_namespace=chrisking
+image_name="registry.ng.bluemix.net/${registry_namespace}/micro-auth:${build_number}"
+
 # Install plugins
 bx plugin install container-service -r Bluemix
 bx plugin install container-registry -r Bluemix
 
 # Login to Bluemix and init plugins
-bx login -a api.ng.bluemix.net -u $cf_email -p $cf_password -c $cf_account -o $cf_org -s $cf_space
+bx login \
+    -a api.ng.bluemix.net \
+    -u $cf_email \
+    -p $cf_password \
+    -c $cf_account \
+    -o $cf_org \
+    -s $cf_space
+
 bx cs init
 bx cr login
 
-docker tag cloudnative/catalog-jenkins registry.ng.bluemix.net/chrisking/catalog:${build_number}
-docker push registry.ng.bluemix.net/chrisking/catalog:${build_number}
+docker push ${image_name}
 
