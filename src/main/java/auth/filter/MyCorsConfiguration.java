44a1f7e4adb7692b034459239813563caf531342
Jeffrey Kwong <jkwong@Jeffreys-MacBook-Pro.local>
2017-04-25 13:57:28 -0400

    CORS, add ingress resource yaml

---
+++ src/main/java/auth/filter/MyCorsConfiguration.java
@@ -0,0 +1,31 @@
+package auth.filter;
+
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class MyCorsConfiguration  {
+
+	@Bean
+    public FilterRegistrationBean corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		
+		final FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+		bean.setOrder(0);
+		
+		return bean;
+	}
+}
