0db7b88dcdafe1d5b748ca15c76e3b001b337442
Jeffrey Kwong <jkwong@Jeffreys-MacBook-Pro.local>
2017-04-25 14:22:35 -0400

    fix CORS configuration to include specific HTTP methods

--- src/main/java/auth/MyCorsConfiguration.java
+++ src/main/java/auth/MyCorsConfiguration.java
@@ -1,30 +1,36 @@
-package auth.filter;
+package auth;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class MyCorsConfiguration  {
+    private static Logger logger =  LoggerFactory.getLogger(MyCorsConfiguration.class);
 
 	@Bean
     public FilterRegistrationBean corsFilter() {
+		logger.info("corsFilter()");
+		
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		final CorsConfiguration config = new CorsConfiguration();
+		
 		config.setAllowCredentials(true);
 		config.addAllowedOrigin("*");
 		config.addAllowedHeader("*");
-		config.addAllowedMethod("*");
+		config.addAllowedMethod("OPTIONS");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("GET");
 		source.registerCorsConfiguration("/**", config);
 		
 		final FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-		bean.setOrder(0);
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
 		
 		return bean;
 	}
