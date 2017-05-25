c84ad4e519d3c8255881a9dd6cccaa437cbc2f6f
Jeffrey Kwong <jkwong@Jeffreys-MacBook-Pro.local>
2017-05-25 13:44:03 -0400

    add healthcheck which checks customer healthcheck

--- src/main/java/auth/AuthController.java
+++ src/main/java/auth/AuthController.java
@@ -2,11 +2,14 @@ package auth;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import auth.customer.CustomerServiceClient;
 
 /**
  * REST Controller to manage Customer database
@@ -17,11 +20,15 @@ public class AuthController {
     
     private static Logger logger =  LoggerFactory.getLogger(AuthController.class);
     
+    @Autowired
+    private CustomerServiceClient customerClient;
+    
     /**
      * check
      */
-    @RequestMapping("/check")
+    @RequestMapping("/healthz")
     @ResponseBody String check() {
+    	customerClient.healthCheck();
         return "it works!";
     }
 
